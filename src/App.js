d86d1b0221895cfe8ebec047e4b1ce65bb90be03
andersonlouzada <anderson.s.louzada@gmail.com>
2023-03-06 18:38:36 -0300

    cria funções no componente App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,131 @@
 import React from 'react';
+import Card from './components/Card';
+import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardImage: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    isSaveButtonDisabled: true,
+    cardsList: [],
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, this.validateButton);
+  };
+
+  validateButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+
+    const validInputsText = (
+      cardName.length === 0
+      || cardDescription.length === 0
+      || cardImage.length === 0
+      || cardRare.length === 0
+    );
+
+    const maxValue = 90;
+    const maxSum = 210;
+    const validInputsNumber = (
+      (Number(cardAttr1) < 0 || Number(cardAttr1) > maxValue)
+      || (Number(cardAttr2) < 0 || Number(cardAttr2) > maxValue)
+      || (Number(cardAttr3) < 0 || Number(cardAttr3) > maxValue)
+    );
+
+    const validSum = (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) > maxSum);
+
+    this.setState(
+      { isSaveButtonDisabled: validInputsText || validInputsNumber || validSum },
+    );
+  };
+
+  saveCards = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo } = this.state;
+
+    this.setState((prevState) => ({ cardsList: [...prevState.cardsList,
+      { cardName,
+        cardDescription,
+        cardImage,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardRare,
+        cardTrunfo }],
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
+    cardImage: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    }));
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
     return (
-      <div>
-        <h1>Tryunfo</h1>
-      </div>
+      <main>
+        <div>
+          <h1>Tryunfo</h1>
+        </div>
+        <Form
+          onInputChange={ this.handleChange }
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardImage={ cardImage }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.saveCards }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardImage={ cardImage }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
+      </main>
     );
   }
 }
